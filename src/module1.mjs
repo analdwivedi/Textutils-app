8e972c7e1f7e6a4b0e9d16d1cb04ed0fa0609768
Noctis <dwivedi.anal@gmail.com>
2024-02-13 11:59:03 +0530

    Initial commit

--- src/module1.mjs
+++
@@ -1,8 +0,0 @@
-const a = 'ichigo';
-const b = 'sado';
-const c = 'inoue';
-
-// default export:-
-export default c;
-// named export:-
-export {a, b};
--- src/module2.mjs
+++
@@ -1,4 +0,0 @@
-import fz, {a, b} from "./module1.mjs"
-
-console.log(fz)
-console.log(a, b)
